8bb38f52c21f6bc79806f91a0fb3bdf6b3dc5acf
RäDev <github@rayraydev.dev>
2022-10-07 15:16:11 -0600

    Final-ish commit.

--- ModuleFiveSDOne/Program.cs
+++ ModuleFiveSDOne/Program.cs
@@ -6,6 +6,7 @@ class Fibonacci
 
     public static void Main(string[] args)
     {
+        Write($"Please select \"1\" to view the iterative example of the Fibonacci sequence. Press \"2\" to view the recursive example of the Fibonacci sequence. ");
         userSelection = Convert.ToInt32(ReadLine());
         if (userSelection == 1)
         {
@@ -18,6 +19,7 @@ class Fibonacci
         else
         {
             WriteLine("You did not make a valid selection. Please relaunch the program and try again.");
+            WaitForKey();
         }
     }
 
@@ -26,6 +28,7 @@ class Fibonacci
         Write("How many results would you like outputted? ");
         int userInput = Convert.ToInt32(ReadLine());
         FibonacciRecursive(0, 1, 1, userInput);
+        WaitForKey();
     }
 
     private static void FibonacciRecursive(Int64 n1, Int64 n2, Int64 n3, int userInput)
@@ -53,4 +56,9 @@ class Fibonacci
         }
     }
 
+    private static void WaitForKey()
+    {
+        WriteLine("Press any key to continue...");
+        ReadKey();
+    }
 }
