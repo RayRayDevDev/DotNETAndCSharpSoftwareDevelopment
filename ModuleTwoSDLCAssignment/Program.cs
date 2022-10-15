3de468d4053098c4ae487122bf010cd379e1c41a
RäDev <github@rayraydev.dev>
2022-10-14 21:14:01 -0600

    New UI. Yikes.

--- ModuleTwoSDLCAssignment/Program.cs
+++ ModuleTwoSDLCAssignment/Program.cs
@@ -1,18 +1,36 @@
-﻿using System.Text.RegularExpressions;
+﻿using ModuleTwoSDLCAssignment;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
 using static System.Console;
+using Application = System.Windows.Application;
+
 //Created by Cole Stanley (RäDev) for CEN 4370C on 09/18/2022.
 //Program takes a text file, converts it into lowercase, parses each word into a list, and loops through the list, counting each unique occurrence of a particular word. The program then publishes the list to the Console. 
 
 internal class Parser
 {
-    public static void Main(string[] args) //Cleaner Main method.
+    public static Application WinApp { get; private set; }
+    public static Window MainWindow { get; private set; }
+
+    public static IEnumerable<string>? matches;
+    static void InitializeWindows()
     {
-        Title = "Occurrence Counter";
-        checkIfFileExists();
-        readAndParseFile();
+        WinApp = new Application();
+        WinApp.Run(MainWindow = new UserControl1()); 
     }
 
-    private static void countOccurences(List<string> regexList) //Pass the List generated in the readAndParseFile into this method.
+    [STAThread]
+    public static void Main(string[] args) //Cleaner Main method.
+    {
+
+        //Title = "Occurrence Counter";
+        //checkIfFileExists();
+        readAndParseFile();
+        InitializeWindows();
+    }
+
+    public static void countOccurences(List<string> regexList) //Pass the List generated in the readAndParseFile into this method.
     {
         var countOccurrences = new SortedDictionary<string, int>(); //SortedDictionary due to Alphabetical sorting when values are the same.
         for (var i = 0; i < regexList.Count; i++)
@@ -26,12 +44,26 @@ internal class Parser
                 countOccurrences.Add(regexList[i], 1);
             }
 
-        foreach (var counts in countOccurrences.OrderByDescending(key => key.Value)) //Initialize a foreach loop that sorts the number of occurrences of any given word in descending order and prints the newly sorted dictionary to the console. 
-            WriteLine($"The word \"{counts.Key}\" was used {counts.Value} time(s) in the document!");
+        /*foreach (var counts in countOccurrences.OrderByDescending(key => key.Value)) //Initialize a foreach loop that sorts the number of occurrences of any given word in descending order and prints the newly sorted dictionary to the console. 
+            WriteLine($"The word \"{counts.Key}\" was used {counts.Value} time(s) in the document!");*/
+        var first = matches.ElementAtOrDefault(0);
+        var numOfMatches = matches.Count();
+        var last = matches.ElementAt(numOfMatches -= 1);
+        UserControl1(first, last);
         WaitForKey();
     }
 
-    private static void readAndParseFile()
+    private static void UserControl1(string? first, string last)
+    {
+        ModuleTwoSDLCAssignment.UserControl1.getVar(first, last);
+    }
+
+    protected static IEnumerable<string> GetMatches()
+    {
+        return matches;
+    }
+
+    protected static void readAndParseFile()
     {
         var inputMixedCaseFile = new string(@"Text_File\The_Raven--EAP.txt");
         var outputToLowercase = new string(@"Text_File\The_Raven--EAP_Output.txt");
@@ -40,12 +72,14 @@ internal class Parser
         File.WriteAllText(outputToLowercase, toLowercase);
         var pattern = @"\w+"; //Regex to disregard whitespace and only look at word characters.
         var regex = new Regex(pattern);
-        var matches = regex.Matches(File.ReadAllText(outputToLowercase)).OfType<Match>().Select(m => m.Value); //Probably redundant, but output the text after reading it to lowercase, just as a safety valve. Select each value output by the Regex expression and output them to a new form.
+        matches = regex.Matches(File.ReadAllText(outputToLowercase)).OfType<Match>().Select(m => m.Value); //Probably redundant, but output the text after reading it to lowercase, just as a safety valve. Select each value output by the Regex expression and output them to a new form.
         matches = new List<string>(matches); //Create a new list and fill the new list from the IEnumerable<string> "matches." 
         countOccurences((List<string>)matches); //Pass the new list "matches" (with a cast since the compiler was yelling at me) into the function which counts each occurrence of a given word. 
+
+        
     }
 
-    private static void checkIfFileExists() //Check if the input text file exists and, if not, display a message and exit the program. If the file does exist, print a message and wait for the user to press any key to continue. 
+    /*private static void checkIfFileExists() //Check if the input text file exists and, if not, display a message and exit the program. If the file does exist, print a message and wait for the user to press any key to continue. 
     {
         var relativePath = Path.Combine(Directory.GetCurrentDirectory(), @"Text_File\The_Raven--EAP.txt");
         if (File.Exists(relativePath))
@@ -59,12 +93,12 @@ internal class Parser
             WaitForKey();
             Environment.Exit(1);
         }
-    }
+    }*/
 
     private static void WaitForKey() //Easier method than calling "ReadKey()" and writing more lines of code as a result. 
     {
         Write("\nPress any key to continue...");
-        ReadKey();
-        Clear();
+        Read();
+        //Clear();
     }
 }
---
+++ ModuleTwoSDLCAssignment/UserControl1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModuleTwoSDLCAssignment
+{
+    public partial class UserControl1 : Window
+    {
+        public static IEnumerable<string>? matches;
+        private static string first;
+        private static string last;
+
+        public static void getVar(string oneFirst, string oneLast)
+        {
+            first = oneFirst;
+            last = oneLast;
+        }
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void clickMeButtonMost(object sender, RoutedEventArgs e)
+        {
+            maxCount.Content = first;
+
+        }
+
+        private void clickMeButtonLeast(object sender, RoutedEventArgs e)
+        {
+            minCount.Content = last;
+        }
+    }
+
+
+}
